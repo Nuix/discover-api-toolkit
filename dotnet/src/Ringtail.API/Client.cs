fbb41d38c01d1cb1444c83465c374badb1c3cd39
Eric Robinson <eric.robinson@fticonsulting.com>
2018-06-29 10:10:10 -0700

    Added dotnet Ringtail.API source

---
+++ dotnet/src/Ringtail.API/Client.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Ringtail.API
+{
+    public class Client
+    {
+        public Configuration Configuration { get; private set; }
+        private HttpClient HttpClient { get; set; }
+
+        public Client(Configuration config)
+        {
+            this.Configuration = config;
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            if (HttpClient != null)
+                return;
+
+            HttpClient = new HttpClient();
+            HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Configuration.Token);
+            HttpClient.DefaultRequestHeaders.Add("ApiKey", Configuration.ApiKey);
+            HttpClient.BaseAddress = new Uri(Configuration.Uri);
+            HttpClient.DefaultRequestHeaders.Accept.Clear();
+            HttpClient.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<HttpResponseMessage> Execute(string command, string operation = null, dynamic variables = null)
+        {
+            var query = new GraphQLQuery() { OperationName=operation, Query = command, Variables = variables };
+            var content = JsonConvert.SerializeObject(query);
+
+            return await HttpClient.PostAsync("", new StringContent(content, Encoding.ASCII, "application/json"));
+        }
+
+        private class GraphQLQuery
+        {
+            public string OperationName { get; set; }
+            public string Query { get; set; }
+            public dynamic Variables { get; set; }
+        }
+    }
+}
---
+++ dotnet/src/Ringtail.API/Configuration.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+namespace Ringtail.API
+{
+    [DataContract]
+    public class Configuration
+    {
+        [DataMember(Name = "name")]
+        public string Name { get; private set; }
+        [DataMember(Name = "token")]
+        public string Token { get; private set; }
+        [DataMember(Name = "apiKey")]
+        public string ApiKey { get; private set; }
+        [DataMember(Name = "uri")]
+        public string Uri { get; private set; }
+
+        public static Configuration Load(string profileName = "default")
+        {
+            var path = ConfigFilePath();
+            var text = File.ReadAllText(path);
+
+            // Allow for an array of configurations
+            try
+            {
+                var configs = JsonConvert.DeserializeObject<List<Configuration>>(text);
+                return configs.First(c => c.Name.Equals(profileName));
+            }
+
+            //...or a single config object (non-array)
+            catch
+            {
+                var config = JsonConvert.DeserializeObject<Configuration>(text);
+                if (config != null && config.Name.Equals(profileName))
+                    return config;
+            }
+            return null;
+        }
+
+        public static string ConfigFilePath()
+        {
+            var home = Environment.GetEnvironmentVariable("USERPROFILE");
+            return Path.Combine(home, @".ringtail\config");
+        }
+    }
+}
